e67ab00b891419e73ad28a220a2233480c75af81
NoniDorcas <kdorcasmuthoni19@gmail.com>
2023-02-21 14:08:03 -0800

    nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,7 @@
 /*
  *function print_alphabet  prints the alphabet in lowercase, followed by a new line.
  */
-int main(void)
-{
-    print_alphabet(void);
-    return (0);
-}
+
 void print_alphabet(void)
 {
 	char lc;
