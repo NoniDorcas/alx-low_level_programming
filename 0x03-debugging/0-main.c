aaf915ff733cac0ae470d1393bf6e7618348e10a
NoniDorcas <kdorcasmuthoni19@gmail.com>
2023-02-23 00:55:52 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main function tests the function positive_or_negative()
+ * returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
