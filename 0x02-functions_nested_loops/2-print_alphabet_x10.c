98756ef0320a83157eb147c3675ef90439c45790
NoniDorcas <kdorcasmuthoni19@gmail.com>
2023-02-22 17:41:22 -0800

    nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet
- * Description: print alphabet 10 times
  * print_alphabet_x10 - function declaration
  * Description: print alphabet in lowercase 10 times
  * Return: void
@@ -11,29 +11,23 @@
 void print_alphabet_x10(void);
 
 /**
- * main - entry point
- * Description: calls print_alphabet_x10
- * Return: 0, success
- */
-
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-/**
  * print_alphabet_x10 - print alphabet 10 times
  * Description: has no arguements
-@@ -37,9 +25,9 @@ void print_alphabet_x10(void)
+ * Return: has no return value
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count = 0;
+
+	while (count <= 9)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
-			putchar(letter);
 			_putchar(letter);
 		}
-		putchar('\n');
 		_putchar('\n');
 		count++;
 	}
-}}
+}
