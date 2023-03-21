54acdad0ae60ee8366c015ae2e7acc039ad6e59d
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-21 08:36:36 -0700

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * file: 4-sum.h
+ * desc: this fails contains the macro SUM(x, y)
+ *       computes the sum of x and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
