5e4a999da16161b5adc375a1ef18a84f4ef2ef5c
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-20 23:12:46 -0700

    task 1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: 1-pi.h
+ * header file that defines a macro named PI as an abbreviation for he token 3.14159265359
+ * it is guarded to prevent double inclusion
+ */
+
+#define PI 3.14159265359
+
+#endif
