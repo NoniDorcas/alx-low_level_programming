b783864eb130db09e7c21327d19d642b070c624e
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-20 23:26:50 -0700

    task 0 and 1

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -2,8 +2,8 @@
 #define OBJECT_LIKE_MACRO_H
 
 /*
- * required to create a header file that defines macro SIZE as an abbreviation for 1024
- * use guards #ifndef, #define and #endif to avoid double inclusion
+ * File: 0-object_like_macro.h
+ * Desc: Defines a macro named SIZE of value 1024
  */
 
 #define SIZE 1024
--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,10 +1,9 @@
 #ifndef PI_H
 #define PI_H
 
-/**
- * file: 1-pi.h
- * header file that defines a macro named PI as an abbreviation for he token 3.14159265359
- * it is guarded to prevent double inclusion
+/*
+ * File: 1-pi.h
+ * Desc: Defines a macro named PI for the token 3.14159265359
  */
 
 #define PI 3.14159265359
