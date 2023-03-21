127aee808962b3e2e9870a038813f86ea8e5e504
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-03-21 08:16:44 -0700

    task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * file: 3-function_like_macro.h
+ * desc: contains a macro ABS(x)
+ * computes absolute value of x
+ */
+
+#define ABS(x) ((x) < 0 ? (-(x)) : (x))
+
+#endif
