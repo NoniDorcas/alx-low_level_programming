3fca9208f7153c69acb49daa7521ab15847829be
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-04 06:21:50 -0700

    task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,36 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new);
+
+/**
+ * _r - reallocates memory to the nodes in a linked list
+ * @list: the previous list to append
+ * @size: size of the new list
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
+ *         98 if failed
+ */
+
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
+{
+	const listint_t **newlist;
+	size_t x;
+
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
+	{
+		free(list);
+		exit(98);
+	}
+	for (x = 0; x < size - 1; x++)
+		newlist[x] = list[x];
+	newlist[x] = new;
+	free(list);
+	return (newlist);
+}
+
 /**
  * print_listint_safe - prints a listint_t linked list.
  * @head: pointer to the start head of the list
