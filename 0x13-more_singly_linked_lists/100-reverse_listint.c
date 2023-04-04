5aa1424d25f573b30eef869930c7092d43754b2a
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-03 23:44:45 -0700

    task 100

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - reverses a linked list
+  * @head: head address
+  * Return: pointer to the first node of the reversed list
+  */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	head++;
+	return (0);
+}
