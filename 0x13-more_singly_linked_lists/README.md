b930203cf4ac3138f7249ce9c0aeda3f62a5b58b
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-03 21:20:42 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains more tasks to be solved to deepen our understanding of linked lists
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stddef.h>
+
+/**
+ * struct listint_s - structure of singly linked list
+ * @n: number of nodes
+ * @next: points to the next node
+ */
+
+typedef struct listint_s
+{
+		int n;
+		struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
