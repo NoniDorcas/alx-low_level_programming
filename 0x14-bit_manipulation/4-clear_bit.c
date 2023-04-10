15bede260bd8b5f631ac7d1e6a2f25b66b4daa99
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-10 09:37:09 -0700

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @index: index starting from 0
+ * @n: binary number
+ * Return: 1 if successful
+ *         -1 if failed
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int x;
+	unsigned int set;
+
+	x = sizeof(n) * 8 - 1;
+	if (index > x)
+	{
+		return (-1);
+	}
+	set = 0 << index;
+	*n |= set;
+
+	return (1);
+}
