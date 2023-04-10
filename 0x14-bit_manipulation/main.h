1f154a40c9a0bd468c22457647cfa1b133d4520e
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-10 07:22:02 -0700

    task 1

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 /**
  *prototype functions to be used in
  * 0x14-bit_manipulation directory tasks.
  */
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
   
