b4b06f9b3a8cf76026bf15eaa23f5c7b3118bc7c
NoniDorcas <kdorcasmuthoni@gmail.com>
2023-04-10 04:32:50 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int dec;
-	int x;
+	int bin;
+	unsigned int x;
 
-	for (dec = 0; x = 0; b[x] != '0/'; x++)
+
+	x = 0;
+	for (bin = 0; b[bin] != '0/'; bin++)
 	{
-	if ( b[x] == 1)
-	dec <<= 1;
-	else if (b[x] == 0)
-	dec <<= 0;
-	else if ( b[x] != '1' && b[x] != '0' )
-	return (0)
+	if (b[bin] == '1')
+	x = 1;
+	else if (b[bin] == '0')
+	x = 0;
+	return (x);
 	}
+	 if ( b[x] != '1' && b[x] != '0' )
+	return (0);
+	
 }
